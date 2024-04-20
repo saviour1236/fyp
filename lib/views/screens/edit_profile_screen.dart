a3a37a415bae8b031f66a95cfe331720a0f5bddc
Saviour Limbu <saviourlimbu@gmail.com>
2024-04-20 21:49:17 +0545

    edit profile completed

--- lib/views/screens/edit_profile_screen.dart
+++ lib/views/screens/edit_profile_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:get/get.dart'; // Assuming you're using GetX for navigation
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
 
 class EditProfileScreen extends StatefulWidget {
   @override
@@ -11,89 +12,86 @@ class EditProfileScreen extends StatefulWidget {
 }
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
-  TextEditingController _usernameController = TextEditingController();
-  File? _profileImage;
-  final ImagePicker _picker = ImagePicker();
+  final TextEditingController _usernameController = TextEditingController();
+  File? _image;
+  final picker = ImagePicker();
 
-  Future<void> pickImage(ImageSource source) async {
-    final pickedFile = await _picker.pickImage(source: source);
-    if (pickedFile != null) {
-      setState(() {
-        _profileImage = File(pickedFile.path);
-      });
-    }
-    Navigator.of(context).pop(); // Close the dialog after selection
+  @override
+  void initState() {
+    super.initState();
+    loadCurrentUserInfo();
   }
 
-  void showOptionsDialog() {
-    showDialog(
-      context: context,
-      builder: (context) => SimpleDialog(
-        title: const Text('Select Image Source'),
-        children: [
-          SimpleDialogOption(
-            onPressed: () => pickImage(ImageSource.gallery),
-            child: const ListTile(
-              leading: Icon(Icons.image),
-              title: Text('Gallery'),
-            ),
-          ),
-          SimpleDialogOption(
-            onPressed: () => pickImage(ImageSource.camera),
-            child: const ListTile(
-              leading: Icon(Icons.camera_alt),
-              title: Text('Camera'),
-            ),
-          ),
-          SimpleDialogOption(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const ListTile(
-              leading: Icon(Icons.cancel),
-              title: Text('Cancel'),
-            ),
-          ),
-        ],
-      ),
-    );
+  void loadCurrentUserInfo() async {
+    var userDoc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+    Map<String, dynamic>? userData = userDoc.data();
+
+    setState(() {
+      _usernameController.text = userData?['name'] ?? '';
+    });
+  }
+
+  Future<void> pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
   }
 
   Future<void> updateProfile() async {
-    try {
-      // Assuming you have a method to get the current user ID
-      String uid = 'currentUserUid'; // Get this from your user management logic
+    Get.dialog(
+      Center(
+          child:
+              CircularProgressIndicator()), // Show loading indicator during the update
+      barrierDismissible:
+          false, // Prevent dismissing the dialog by tapping outside
+    );
 
-      // Update the username
-      await FirebaseFirestore.instance.collection('users').doc(uid).update({
-        'username': _usernameController.text,
-      });
-
-      // Update the profile picture if a new one was picked
-      if (_profileImage != null) {
-        String fileName =
-            'profile_${uid}_${DateTime.now().millisecondsSinceEpoch}';
-        FirebaseStorage storage = FirebaseStorage.instance;
-        // Upload the new profile picture
-        await storage.ref('profilePics/$fileName').putFile(_profileImage!);
-        String imageUrl =
-            await storage.ref('profilePics/$fileName').getDownloadURL();
-
-        // Update the user's profile image URL in Firestore
-        await FirebaseFirestore.instance.collection('users').doc(uid).update({
-          'profilePic': imageUrl,
-        });
-      }
-
-      // Navigate back to the ProfileScreen after updates are successful
-      Get.back(); // Using GetX for navigation
-    } catch (e) {
-      print('Error updating profile: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Failed to update profile: $e'),
-          backgroundColor: Colors.red,
-        ),
-      );
+    String profileUrl = '';
+    if (_image != null) {
+      var storageReference = FirebaseStorage.instance
+          .ref()
+          .child('profilePictures/${FirebaseAuth.instance.currentUser!.uid}');
+      await storageReference.putFile(_image!);
+      profileUrl = await storageReference.getDownloadURL();
     }
+
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .update({
+      'name': _usernameController.text,
+      'profilePhoto': profileUrl.isEmpty
+          ? (FirebaseAuth.instance.currentUser!.photoURL ?? '')
+          : profileUrl,
+    }).then((value) {
+      Get.back();
+      Get.back();
+      Get.snackbar(
+        'Profile Updated',
+        'Your profile has been successfully updated!',
+        snackPosition: SnackPosition.BOTTOM,
+        colorText: Colors.white,
+        duration: Duration(seconds: 3),
+      );
+    }).catchError((error) {
+      Get.back();
+      Get.snackbar(
+        'Update Failed',
+        'Failed to update your profile. Please try again.',
+        snackPosition: SnackPosition.TOP,
+        colorText: Colors.white,
+        duration: Duration(seconds: 3),
+      );
+    });
   }
 
   @override
@@ -105,27 +103,38 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            if (_profileImage != null)
-              Image.file(_profileImage!, height: 200, width: 200),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () => pickImage(),
+              child: CircleAvatar(
+                radius: 60,
+                backgroundImage: _image != null
+                    ? FileImage(_image!) as ImageProvider
+                    : FirebaseAuth.instance.currentUser!.photoURL != null
+                        ? NetworkImage(
+                                FirebaseAuth.instance.currentUser!.photoURL!)
+                            as ImageProvider
+                        : null,
+                child: _image == null &&
+                        FirebaseAuth.instance.currentUser!.photoURL == null
+                    ? Icon(Icons.add_photo_alternate, size: 60)
+                    : null,
+              ),
+            ),
+            SizedBox(height: 20),
             TextField(
               controller: _usernameController,
               decoration: InputDecoration(
                 labelText: 'Username',
+                border: OutlineInputBorder(),
               ),
             ),
-            const SizedBox(height: 20),
-            ListTile(
-              title: Text(_profileImage == null
-                  ? 'No Image Selected'
-                  : 'Image Selected'),
-              trailing: Icon(Icons.edit),
-              onTap: showOptionsDialog,
-            ),
-            const SizedBox(height: 20),
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: updateProfile,
-              child: Text('Update'),
+              onPressed: () => updateProfile(),
+              child: Text('Update Profile'),
             ),
+            SizedBox(height: 20),
           ],
         ),
       ),
